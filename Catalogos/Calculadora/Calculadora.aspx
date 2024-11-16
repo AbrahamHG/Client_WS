f746113ab1aa15df7f661e970e2f21acb3d6e793
AbrahamHG <ahernandez@bajaware.com>
2024-11-16 10:20:19 -0600

    Agregar archivos de proyecto.

---
+++ Catalogos/Calculadora/Calculadora.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Calculadora.aspx.cs" Inherits="Client_WS.Catalogos.Calculadora.Calculadora" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <%--Campos de Texto--%>
+    <div class="col.4">
+        <asp:Label ID="lbla" runat="server" Text="Numero 1"></asp:Label>
+        <asp:TextBox ID="txta" runat="server" TextMode="Number"></asp:TextBox>
+
+    </div>
+    <br />
+    <div class="col4">
+        <asp:Label ID="lblb" runat="server" Text="Numero 2"></asp:Label>
+        <asp:TextBox ID="txtb" runat="server" TextMode="Number"></asp:TextBox>
+    </div>
+    
+
+    <div class="row text-center">
+        <%--Botones--%>
+        <asp:Button ID="btnSumar" runat="server" Text="+" OnClick="btnSumar_Click" CssClass="m-5 col" />
+        <asp:Button ID="btRestar" runat="server" Text="-" OnClick="btRestar_Click" CssClass="m-5 col" />
+        <asp:Button ID="btnMultiplicar" runat="server" Text="*" OnClick="btnMultiplicar_Click" CssClass="m-5 col" />
+        <asp:Button ID="btnDividir" runat="server" Text="/" OnClick="btnDividir_Click" CssClass="m-5 col" />
+
+
+    </div>
+    <div class="row">
+        <div class="text-center">
+            <h2>
+                <asp:Label ID="lblresultado" runat="server" Text=""></asp:Label>
+            </h2>
+
+
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ Catalogos/Calculadora/Calculadora.aspx.cs
@@ -0,0 +1,68 @@
+﻿using Client_WS.CalculadoraService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client_WS.Catalogos.Calculadora
+{
+    public partial class Calculadora : System.Web.UI.Page
+    {
+        //creamos un cliente que resulva las peticiones del servicio SOAP
+        calculadora_ServiceSoapClient cliente_WS;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Inicializo mi cliente SOAP
+            cliente_WS = new calculadora_ServiceSoapClient();
+
+
+        }
+
+        protected void btnSumar_Click(object sender, EventArgs e)
+        {
+            //Recupero los datos de mi formulario 
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+            //Invoco mi servicio pasandole los datos que requiere
+            double resultado = cliente_WS.Sum(a, b);
+            //muestro el resultado del servicio en mi pagina
+            lblresultado.Text = resultado.ToString();
+
+        }
+
+        protected void btRestar_Click(object sender, EventArgs e)
+        {
+            //Recupero los datos de mi formulario 
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+            //Invoco mi servicio pasandole los datos que requiere
+            double resultado = cliente_WS.Resta(a, b);
+            lblresultado.Text = resultado.ToString();
+
+
+        }
+
+        protected void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            //Recupero los datos de mi formulario 
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+            //Invoco mi servicio pasandole los datos que requiere
+            double resultado = cliente_WS.Multiplicacion(a, b);
+            lblresultado.Text = resultado.ToString();
+        }
+
+        protected void btnDividir_Click(object sender, EventArgs e)
+        {
+            //Recupero los datos de mi formulario 
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+            //Invoco mi servicio pasandole los datos que requiere
+            double resultado = cliente_WS.Division(a, b);
+            lblresultado.Text = resultado.ToString();
+        }
+    }
+}
